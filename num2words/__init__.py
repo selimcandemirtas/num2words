82ccbe35b55b6f72f5ce5fa68ce79aa63846b0a2
Selim Can Demirtaş <111413623+selimcandemirtas@users.noreply.github.com>
2022-09-13 14:50:36 +0300

    Update __init__.py

--- num2words/__init__.py
+++ num2words/__init__.py
@@ -77,7 +77,7 @@ CONVERTER_CLASSES = {
 CONVERTES_TYPES = ['cardinal', 'ordinal', 'ordinal_num', 'year', 'currency']
 
 
-def num2words_fc(number, ordinal=False, lang='en', to='cardinal', **kwargs):
+def num2words(number, ordinal=False, lang='en', to='cardinal', **kwargs):
     # We try the full language first
     if lang not in CONVERTER_CLASSES:
         # ... and then try only the first 2 letters
